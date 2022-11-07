a3b8ddc73d352d5b1a6b5a27d2fff837cc6f3c6c
Anne Zhang <annezhang@MacBook.local>
2022-11-07 00:34:59 -0800

    finished lab7

--- homeworks/hw7/hw7/assets/js/script.js
+++ homeworks/hw7/hw7/assets/js/script.js
@@ -2,12 +2,21 @@ function getRandomNum {
 
 }
 
-function averageThreeNumbers(5, 3, 1) {
-    let sum = 9;
-    let 3 = "avg";
-    return avg;
-}
+console.log (getRandomNum)
 
-function createSentence {
+let a = 5
+let b = 2
+let c = 1  
+
+function averageThreeNumbers(a, b, c) { 
+    let sum = a+b+c;
+    let "avg"= sum; 
+    return avg/3;
+}
+console.log (averageThreeNumbers);
+
+function createSentence (9, apples) {
+    On average, a Berkeley student has [num] [noun]s;
 
 }
+console.log (createSentence)
