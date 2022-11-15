c693e0f9d878faec5c2faf39114b5e5a31cb8e43
Anne Zhang <annezhang@MacBook.local>
2022-11-14 18:39:29 -0800

    finished lab8

---
+++ labs/lab8/script.js
@@ -0,0 +1,6 @@
+function toggleTheme() {
+    let body = document.querySelector("body");
+    body.classList.toggle("dark-mode");
+}
+let buttonVariable = document.getElementById("toggleButton");
+buttonVariable.onclick = toggleTheme; 
