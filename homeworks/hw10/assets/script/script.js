de5a65613ef409d2b5626b5314835d872b628b2a
unknown <azhang1888@berkeley.edu>
2022-12-01 00:15:56 -0800

    finished hw10

--- homeworks/hw10/assets/script/script.js
+++ homeworks/hw10/assets/script/script.js
@@ -1,8 +1,21 @@
 // s1 outfit
 
-document.getElementsByClassName("outfit");
+function noClothes() {
+	document.getElementById('o1').classList.add('hidden');
+	document.getElementById('o2').classList.add('hidden');
+	document.getElementById('o3').classList.add('hidden');
+	document.getElementById('o4').classList.add('hidden');
+	document.getElementById('o5').classList.add('hidden');
+};
 
-let s1 = document.getElementById("o1"); 
+let strip = document.getElementById('strip-button');
+
+strip.onclick = noClothes;
+
+
+document.getElementsByClassName('outfit');
+
+let s1 = document.getElementById('o1'); 
 	/* get #s1 outfit */
 
 /* the process: */
@@ -12,34 +25,75 @@ let s1 = document.getElementById("o1");
 	(remember: get the list of .outfit elements and use a for loop to hide each outfit)
 	/* 3. show #o1 outfit (set display to block)*/
 
-s1.onclick = function() {
-	let fit = document.getElementsByClassName("fit");
-	fit.classList.add("hidden");
-	s1.classList.toggle("show");
-};
+function out1 () {
+	document.getElementById('o2').classList.add('hidden');
+	document.getElementById('o3').classList.add('hidden');
+	document.getElementById('o4').classList.add('hidden');
+	document.getElementById('o5').classList.add('hidden');
+	document.getElementById('body').classList.add('hidden');
+	document.getElementById('o1').classList.add('show');
+}
+
+document.getElementById('s1').onclick = out1;
 
 // s2 outfit
 /* get #s2 outfit */
-/* repeat process */
+
+function out2 () {
+	document.getElementById('o1').classList.add('hidden');
+	document.getElementById('o3').classList.add('hidden');
+	document.getElementById('o4').classList.add('hidden');
+	document.getElementById('o5').classList.add('hidden');
+	document.getElementById('body').classList.add('hidden');
+	document.getElementById('o2').classList.add('show');
+}
+
+document.getElementById('s2').onclick = out2;
 
 // s3 outfit
 /* get #s3 outfit */
-/* repeat process */
+
+function out3 () {
+	document.getElementById('o1').classList.add('hidden');
+	document.getElementById('o2').classList.add('hidden');
+	document.getElementById('o4').classList.add('hidden');
+	document.getElementById('o5').classList.add('hidden');
+	document.getElementById('body').classList.add('hidden');
+	document.getElementById('o3').classList.add('show');
+}
+
+document.getElementById('s3').onclick = out3;
 
 // s4 outfit
 /* get #s4 outfit */
-/* repeat process */
+
+function out4 () {
+	document.getElementById('o1').classList.add('hidden');
+	document.getElementById('o2').classList.add('hidden');
+	document.getElementById('o3').classList.add('hidden');
+	document.getElementById('o5').classList.add('hidden');
+	document.getElementById('body').classList.add('hidden');
+	document.getElementById('o4').classList.add('show');
+}
+
+document.getElementById('s4').onclick = out4;
 
 // s5 outfit
 /* get #s5 outfit */
-/* repeat process */
+
+function out5 () {
+	document.getElementById('o1').classList.add('hidden');
+	document.getElementById('o2').classList.add('hidden');
+	document.getElementById('o3').classList.add('hidden');
+	document.getElementById('o4').classList.add('hidden');
+	document.getElementById('body').classList.add('hidden');
+	document.getElementById('o5').classList.add('show');
+}
+
+document.getElementById('s5').onclick = out5;
 
 // strip outfit
-let strip = document.getElementsByClassName("strip-button"); /* get #strip-button */
-strip.onclick = function() {
-	let fit = document.getElementsByClassName("outfit");
-	fit.classList.add("hidden");
-};
+
 
 /* call the function you created to hide all the outfits */
 	/* show #body */
